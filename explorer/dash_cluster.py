2d1d529e1549512f9b82100f637c1236788bd2db
Damien Garaud <damien.garaud@oslandia.com>
2019-01-18 14:59:57 +0100

    dash: add a dropdown in order to select the PCA(x, y)

--- explorer/dash_cluster.py
+++ explorer/dash_cluster.py
@@ -11,44 +11,62 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 from data import read_pca, read_cluster, cluster_scatter, pca_arrow, rayon_estimation
 
+print("read files")
+centroid, cluster = read_cluster()
+pcafeatures, pcaind = read_pca()
 
-if __name__ == '__main__':
-    print("read files")
-    centroid, cluster = read_cluster()
-    pcafeatures, pcaind = read_pca()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
 
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
+    html.H2("Cluster / PCA"),
+    dcc.Dropdown(
+        id='pca-dropdown',
+        options=[
+            {'label': 'PC1 / PC2', 'value': 'PC1,PC2'},
+            {'label': 'PC1 / PC3', 'value': 'PC1,PC3'},
+            {'label': 'PC2 / PC3', 'value': 'PC2,PC3'},
+            {'label': 'PC1 / PC4', 'value': 'PC1,PC4'},
+            {'label': 'PC2 / PC4', 'value': 'PC2,PC4'},
+            {'label': 'PC3 / PC4', 'value': 'PC3,PC4'}
+        ],
+        value='PC1,PC2'
+    ),
+    dcc.Graph(
+        id='cluster-pca'
+    )
+])
 
-    # PC1, PC2
-    # first, second = 1, 2
-    first, second = 1, 3
+
+@app.callback(Output('cluster-pca', 'figure'),
+              [Input('pca-dropdown', 'value')])
+def update_cluster_graph(dropdown_value):
+    first, second = dropdown_value.split(",")
     rayon = rayon_estimation(cluster, first, second)
     data = cluster_scatter(cluster, first, second)
-    data += pca_arrow(pcafeatures, first, second, rayon, k=8)
-
-    app.layout = html.Div([
-        dcc.Graph(
-            id='cluster-pca',
-            figure={
-                'data': data,
-                'layout': go.Layout(
-                    xaxis={'title': first},
-                    yaxis={'title': second},
-                    width=800,
-                    height=500,
-                    margin={'l': 100, 'b': 40, 't': 10, 'r': 10},
-                    # legend={'x': 0, 'y': 1},
+    data += pca_arrow(pcafeatures, first, second, rayon, k=6)
+    return {
+        'data': data,
+        'layout': go.Layout(
+            xaxis={'title': "PC{}".format(first)},
+            yaxis={'title': "PC{}".format(second)},
+            width=800,
+            height=500,
+            # margin={'l': 100, 'b': 40, 't': 10, 'r': 10},
+                    margin={'l': 30, 'b': 20, 't': 30, 'r': 20},
+            # legend={'x': 0, 'y': 1},
                     hovermode='closest'
-                )
-            }
         )
-    ])
+    }
 
+
+if __name__ == '__main__':
     app.run_server(debug=True)
