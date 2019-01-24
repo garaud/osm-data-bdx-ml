0c8841c0b058b909f8581310a2334ed440db01e3
Damien Garaud <damien.garaud@oslandia.com>
2019-01-24 15:50:40 +0100

    update the setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='osm-data-bdx-ml',
     packages=find_packages(),
+    install_requires=["jupyter", "jupyter_contrib_nbextensions", "pandas", "geopandas", "table",
+                      "dash", "plotly", "seaborn", "folium"],
     version='0.1.0',
     description="Bordeaux Machine Learning Meetup with OSM data",
     url="https://github.com/garaud/osm-data-bdx-ml",
